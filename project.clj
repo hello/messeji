c7b8d435ed872b415f0b2da871984b2e021f7417
jakepic1 <jake@sayhello.com>
2016-01-21 10:44:20 -0800

    Project skeleton with compiled Java protobuf

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
 (defproject com.hello/messeji "0.1.0-SNAPSHOT"
-:description "Async messaging service for communicating with Sense"
-:url "https://github.com/hello/messeji"
-:dependencies [[org.clojure/clojure "1.7.0"]
-               [aleph "0.4.1-beta2"]
-               [compojure "1.4.0"]
-               [com.hello.suripu/suripu-service "0.6.65"]]
-:plugins [[s3-wagon-private "1.2.0"]]
-:repositories [["private" {:url "s3p://hello-maven/release/"
-                           :username :env
-                           :passphrase :env}]])
+  :description "Async messaging service for communicating with Sense"
+  :url "https://github.com/hello/messeji"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [aleph "0.4.1-beta2"]
+                 [compojure "1.4.0"]
+                 [com.hello.suripu/suripu-service "0.6.65"]]
+  :plugins [[s3-wagon-private "1.2.0"]]
+  :source-paths ["src" "src/main/clojure"]
+  :java-source-paths ["src/main/java"]  ; Java source is stored separately.
+  :test-paths ["test" "src/test/clojure"]
+  :repositories [["private" {:url "s3p://hello-maven/release/"
+                             :username :env
+                             :passphrase :env}]])
