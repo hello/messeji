ef263b57f668403f5887161636dd09404b45e2f8
jakepic1 <jake@sayhello.com>
2016-02-17 16:18:07 -0800

    Simpler release

--- release.sh
+++ release.sh
@@ -1,5 +1,3 @@
-VERSION_QUOTED=`lein release $1`
-TEMP="${$VERSION_QUOTED%\"}"
-VERSION="${TEMP#\"}"
+VERSION=$1
 DEB="messeji_"$VERSION"_all.deb"
 lein deploy packages com.hello/messeji $VERSION $DEB
