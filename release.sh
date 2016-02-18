9f4bc5b5eee090affe3b4c8313597f50b77bef6c
jakepic1 <jake@sayhello.com>
2016-02-18 14:07:43 -0800

    Echo

--- release.sh
+++ release.sh
@@ -4,8 +4,11 @@ VERSION_QUOTED=`lein pprint :version`
 TEMP="${VERSION_QUOTED%\"}"
 VERSION="${TEMP#\"}"
 echo "New version " $VERSION
-# VERSION=$1
+
 DEB="messeji_"$VERSION"_all.deb"
+echo "Uploading deb package to S3: " $DEB
 s3cmd put $DEB "s3://hello-deploy/packages/com/hello/messeji/"$VERSION"/messeji_"$VERSION"_amd64.deb"
 rm *.deb
+
+echo "Preparing new development version."
 lein dev-version
