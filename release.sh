8f8c80a16fd7c47742c05ad5cd97dd7bd15488f0
jakepic1 <jake@sayhello.com>
2016-02-18 14:10:51 -0800

    Uberjar and configs

--- release.sh
+++ release.sh
@@ -10,5 +10,12 @@ echo "Uploading deb package to S3: " $DEB
 s3cmd put $DEB "s3://hello-deploy/packages/com/hello/messeji/"$VERSION"/messeji_"$VERSION"_amd64.deb"
 rm *.deb
 
+echo "Deploying uberjar to maven repo"
+lein deploy-uberjar
+
+echo "Uploading configs"
+s3cmd put resources/config/prod.edn s3://hello-deploy/configs/com/hello/messeji/$VERSION/messeji.prod.edn
+s3cmd put resources/config/staging.edn s3://hello-deploy/configs/com/hello/messeji/$VERSION/messeji.staging.edn
+
 echo "Preparing new development version."
 lein dev-version
