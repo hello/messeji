ba1d7c78283f5f85a8f9999af327ac10c8f4d2ef
jakepic1 <jake@sayhello.com>
2016-03-29 11:27:51 -0700

    Comment out deploy-dev lines

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -6,5 +6,5 @@ ssh messeji-dev "/home/ubuntu/build/download.sh $VERSION --force"
 echo "Deploying $VERSION"
 ssh messeji-dev "sudo /home/ubuntu/build/deploy.sh $VERSION"
 
-echo "Tailing logs; feel free to kill this anytime."
-ssh messeji-dev "tail -f /var/log/suripu-messeji/suripu-messeji.log"
+# echo "Tailing logs; feel free to kill this anytime."
+# ssh messeji-dev "tail -f /var/log/suripu-messeji/suripu-messeji.log"
