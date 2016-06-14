c71b7a867a5c34fd0d4a65b6bae36dd6aa7fea81
jakepic1 <jake@sayhello.com>
2016-06-14 14:04:11 -0700

    Version 0.2.28-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.hello/messeji "0.2.27"
+(defproject com.hello/messeji "0.2.28-SNAPSHOT"
   :description "Async messaging service for communicating with Sense"
   :url "https://github.com/hello/messeji"
   :dependencies [[org.clojure/clojure "1.7.0"]
